106946a9f0aa18f0748c22d6986504a25f1d6889
Brian Akers <brianakers@Brians-MacBook-Pro.local>
2017-09-28 11:29:27 -0400

    dropdown menu for cost curve graph

--- react/src/components/CostCurveTile.js
+++ react/src/components/CostCurveTile.js
@@ -5,8 +5,33 @@ class CostCurveTile extends Component {
   constructor(props){
     super(props);
     this.state = {
-      data:[]
+      data:[],
+      xParams: 100,
+      yParams: 100,
     }
+    this.setCurveParamsAll=this.setCurveParamsAll.bind(this)
+    this.setCurveParams50=this.setCurveParams50.bind(this)
+    this.setCurveParams10=this.setCurveParams10.bind(this)
+    this.setCurveParams5=this.setCurveParams5.bind(this)
+  }
+  setCurveParamsAll(){
+    this.setState({ xParams: 100 })
+    this.setState({ yParams: 100 })
+  }
+
+  setCurveParams50(){
+    this.setState({ xParams: 50 })
+    this.setState({ yParams: 85 })
+  }
+
+  setCurveParams10(){
+    this.setState({ xParams: 10 })
+    this.setState({ yParams: 30 })
+  }
+
+  setCurveParams5(){
+    this.setState({ xParams: 5 })
+    this.setState({ yParams: 20 })
   }
 
   componentDidMount() {
@@ -29,21 +54,32 @@ class CostCurveTile extends Component {
 
   render() {
     return (
-      <AreaChart
-        axes
-        grid
-        verticalGrid
-        xDomainRange={[0, 50]}
-        yDomainRange={[0, 50]}
-        margin={{top: 30, right: 30, bottom: 30, left: 30}}
-        areaColors={['#FFB629']}
-        width={800}
-        height={400}
-        data={[
-          this.state.data
-        ]}
-      />
+      <div>
+        <div className="curveToggle">
+          <a data-dropdown="drop2" aria-controls="drop2" aria-expanded="false">Select a Specific Subset via Cost</a>
+          <div id="drop2" data-dropdown-content className="f-dropdown content" aria-hidden="true" tabIndex="-1">
+          <p onClick={this.setCurveParamsAll}>All Patients</p>
+          <p onClick={this.setCurveParams50}>Top 50% of Patients</p>
+          <p onClick={this.setCurveParams10}>Top 10% of Patients</p>
+          <p onClick={this.setCurveParams5}>Top 5% of Patients</p>
+          </div>
+        </div>
 
+        <AreaChart
+          axes
+          grid
+          verticalGrid
+          xDomainRange={[0, this.state.xParams]}
+          yDomainRange={[0, this.state.yParams]}
+          margin={{top: 30, right: 30, bottom: 30, left: 30}}
+          areaColors={['#FFB629']}
+          width={800}
+          height={400}
+          data={[
+            this.state.data
+          ]}
+        />
+      </div>
     )
   }
 }
