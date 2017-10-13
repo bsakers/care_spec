51f84a464338dbfb905b23b957d79238724a3eea
Brian Akers <brianakers@Brians-MacBook-Pro.local>
2017-10-12 20:32:44 -0400

    updated model tests

--- spec/models/admission_procedure_spec.rb
+++ spec/models/admission_procedure_spec.rb
@@ -2,13 +2,11 @@ require 'rails_helper'
 
 RSpec.describe AdmissionProcedure, type: :model do
   describe AdmissionProcedure do
-    # it { should belong_to :diagnosis }
-    # it { should belong_to :patient }
 
-    # it { should have_valid(:procedure).when(Procedure.new) }
-    # it { should_not have_valid(:procedure).when(nil) }
-    #
-    # it { should have_valid(:admission).when(Diagnosis.new) }
-    # it { should_not have_valid(:admission).when(nil) }
+    it { should have_valid(:procedure).when(Procedure.new) }
+    it { should_not have_valid(:procedure).when(nil) }
+
+    it { should have_valid(:admission).when(Admission.new) }
+    it { should_not have_valid(:admission).when(nil) }
   end
 end
--- spec/models/patient_diagnosis_spec.rb
+++ spec/models/patient_diagnosis_spec.rb
@@ -2,9 +2,6 @@ require 'rails_helper'
 
 RSpec.describe PatientDiagnosis, type: :model do
   describe PatientDiagnosis do
-    # it { should belong_to :diagnosis }
-    # it { should belong_to :patient }
-
     it { should have_valid(:patient).when(Patient.new) }
     it { should_not have_valid(:patient).when(nil) }
 
